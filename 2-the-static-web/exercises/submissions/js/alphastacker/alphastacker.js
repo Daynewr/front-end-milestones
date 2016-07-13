78f0360ec6746722faf7dd2ca2014e99ebd23825
Dayne Wright <daynewr@gmail.com>
2016-07-13 09:24:54 -0500

    added forEach to handle space after 5th letter

--- 2-the-static-web/exercises/submissions/js/alphastacker/alphastacker.js
+++ 2-the-static-web/exercises/submissions/js/alphastacker/alphastacker.js
@@ -1,6 +1,12 @@
 const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 let stackLetter = function(letters){
+	letters.forEach(function(v,i){
+		if(i % 5 === 0 && i !== 0){
+			letters.splice(i, 0, " ");
+		}
+	});
+
 	let letterString = letters.join('');
 	let answer;
 
